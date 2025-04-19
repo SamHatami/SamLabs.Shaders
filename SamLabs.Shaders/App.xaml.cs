2b2f39a700d8d4a4247e62c5ff6738574ba0d2c3
Sam <mhatami@hotmail.com>
2025-04-19 19:24:19 +0200

    OpenTK

---
+++ SamLabs.Shaders/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SamLabs.Shaders
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ SamLabs.Shaders/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using OpenTK.Wpf;
+using System.Windows;
+
+namespace SamLabs.Shaders
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var settings = new GLWpfControlSettings
+            {
+                MajorVersion = 4,
+                MinorVersion = 3
+            };
+            OpenTkControl.Start(settings);
+        }
+
+        private void OpenTkControl_OnRender(TimeSpan delta)
+        {
+            GL.ClearColor(Color4.Blue);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+        }
+    }
+}
